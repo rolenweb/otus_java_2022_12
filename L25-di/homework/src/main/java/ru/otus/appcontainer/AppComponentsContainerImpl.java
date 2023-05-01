9c208d7c2e0e65d57d340a730c8c0ce2d24c4a53
Roman Khrustalev <rolenweb@yandex.ru>
2023-05-01 21:31:22 +0300

    L25 DI

--- L25-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ L25-di/homework/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,22 +1,55 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.TypeVariable;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
+    private final Object initialConfigInstance;
     public AppComponentsContainerImpl(Class<?> initialConfigClass) {
+        try {
+            this.initialConfigInstance = initialConfigClass.getConstructor().newInstance();
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException | InstantiationException e) {
+            throw new RuntimeException(e);
+        }
         processConfig(initialConfigClass);
     }
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+        var methods =  Arrays.stream(configClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(AppComponent.class))
+                .sorted(Comparator.comparingInt(m -> m.getAnnotation(AppComponent.class).order()))
+                .toList();
+        for (Method method: methods) {
+            var annotation = method.getAnnotation(AppComponent.class);
+            if (appComponentsByName.containsKey(annotation.name())) {
+                throw new RuntimeException(String.format("The component %s has been already presented in the context", annotation.name()));
+            }
+            List<Object> methodParameters = new ArrayList<>();
+            for (Class<?> parameter: method.getParameterTypes()) {
+                var component = getAppComponent(parameter);
+                methodParameters.add(component);
+            }
+            try {
+                Object component = method.invoke(initialConfigInstance, methodParameters.toArray());
+                appComponentsByName.put(annotation.name(), component);
+                appComponents.add(component);
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -27,11 +60,24 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+         var components = appComponents
+                .stream()
+                .filter(c -> componentClass.isAssignableFrom(c.getClass())).toList();
+        if ((long) components.size() > 1) {
+            throw new RuntimeException("There are doubles");
+        }
+        if (components.isEmpty()) {
+            throw new NoSuchElementException("No value present");
+        }
+
+        return (C) components.get(0);
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        if (!appComponentsByName.containsKey(componentName)) {
+            throw new NoSuchElementException("No value present");
+        }
+        return (C) appComponentsByName.get(componentName);
     }
 }
